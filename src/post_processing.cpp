ebc87d1d4d6c4e18b91e0f741ccb73c675de5148
William Narchi <w.narchi-1@student.tudelft.nl>
2020-10-29 02:29:32 +0100

    Add bloom filter
    The current implementation looks strange on the Cornell Box, but that is likely due to the similarity in colour values on each box face. This will probably be fixed if interpolation with barycentric coordinates is implemented

--- src/post_processing.cpp
+++ src/post_processing.cpp
@@ -2,10 +2,15 @@
 
 const bool POST_PROCESS = true; // Enable/disable post-processing.
 
+// Bloom filter parameters.
+const float BLOOM_THRESHOLD = 0.95; // Threshold to use for filtering 'bright' pixels.
+const int BLOOM_BLUR_SIZE = 3;      // Size to use for the box filter used for blurring.
+
 void postProcessingPipeline(Screen &screen, const glm::ivec2 &windowResolution, std::vector<std::vector<glm::vec3>> &pixel_array)
 {
     if (POST_PROCESS)
     {
+        bloomFilter(windowResolution, pixel_array, BLOOM_THRESHOLD, BLOOM_BLUR_SIZE);
     }
 
 #ifdef USE_OPENMP
@@ -24,37 +29,69 @@ void postProcessingPipeline(Screen &screen, const glm::ivec2 &windowResolution, 
 std::vector<std::vector<glm::vec3>> boxBlur(const glm::ivec2 &windowResolution, std::vector<std::vector<glm::vec3>> &pixel_array,
                                             float threshold, int filter_size)
 {
-    // Create suitably sized 2D vector to house blurred pixel values.
-    std::vector<std::vector<glm::vec3>> blur_array;
-    blur_array.resize(windowResolution.y);
+    // Create suitably sized 2D vector to house filtered pixel values.
+    std::vector<std::vector<glm::vec3>> filtered_pixels;
+    filtered_pixels.resize(windowResolution.y);
 
-    // Process blur for every pixel.
+#ifdef USE_OPENMP
+#pragma omp parallel for
+#endif
+    // Populate filtered_pixels with pixel values that match or exceed the threshold.
     for (int y = 0; y < windowResolution.y; y++)
     {
         for (int x = 0; x < windowResolution.x; x++)
         {
-            // Compute blur if pixel intensity exceeds threshold.   
             float pixel_intensity = glm::length(pixel_array[y][x]);
-            if (pixel_intensity > threshold)
+            glm::vec3 filtered_value = pixel_intensity >= threshold ? pixel_array[y][x] : glm::vec3{0.0f};
+            filtered_pixels[y].push_back(filtered_value);
+        }
+    }
+
+    // Create suitably sized 2D vector to house blurred pixel values.
+    std::vector<std::vector<glm::vec3>> blur_pixels;
+    blur_pixels.resize(windowResolution.y);
+
+#ifdef USE_OPENMP
+#pragma omp parallel for
+#endif
+    // Process blur for every pixel in filtered_pixels.
+    for (int y = 0; y < windowResolution.y; y++)
+    {
+        for (int x = 0; x < windowResolution.x; x++)
+        {
+            // Compute blur.
+            glm::vec3 pixel_sum{0.0f};
+            for (int filter_y = -filter_size; filter_y <= filter_size; filter_y++)
             {
-                glm::vec3 pixel_sum = {0.0f, 0.0f, 0.0f};
-                int valid_samples = 0;
-                for (int filter_y = -filter_size; filter_y <= filter_size; filter_y++)
+                for (int filter_x = -filter_size; filter_x <= filter_size; filter_x++)
                 {
-                    for (int filter_x = -filter_size; filter_x <= filter_size; filter_x++)
+                    int neighbour_y = y + filter_y;
+                    int neighbour_x = x + filter_x;
+                    if (!(neighbour_y < 0) && (neighbour_y < windowResolution.y) && !(neighbour_x < 0) && (neighbour_x < windowResolution.x))
                     {
-                        int neighbour_y = y + filter_y;
-                        int neighbour_x = x + filter_x;
-                        if (!(neighbour_y < 0) && !(neighbour_x < 0))
-                        {
-                            pixel_sum += pixel_array[neighbour_y][neighbour_x];
-                            valid_samples++;
-                        }
+                        pixel_sum += filtered_pixels[neighbour_y][neighbour_x];
                     }
                 }
-                pixel_sum /= valid_samples;
-                blur_array[y][x] = pixel_sum;
             }
+            pixel_sum /= (2 * filter_size + 1) * (2 * filter_size + 1);
+            blur_pixels[y].push_back(pixel_sum);
+        }
+    }
+    return blur_pixels;
+}
+
+void bloomFilter(const glm::ivec2 &windowResolution, std::vector<std::vector<glm::vec3>> &pixel_array, float threshold, int blur_filter_size)
+{
+    std::vector<std::vector<glm::vec3>> blurred_pixels = boxBlur(windowResolution, pixel_array, threshold, blur_filter_size);
+
+#ifdef USE_OPENMP
+#pragma omp parallel for
+#endif
+    for (int y = 0; y < windowResolution.y; y++)
+    {
+        for (int x = 0; x < windowResolution.x; x++)
+        {
+            pixel_array[y][x] += blurred_pixels[y][x];
         }
     }
 }
